e7833e5275c228ba4616b5c702a81b592e9275a2
jab7347 <148905757+jab7347@users.noreply.github.com>
2026-04-06 09:16:34 -0400

    Initial Test Docs

---
+++ helloWorld.py
@@ -0,0 +1,27 @@
+# Dr. Kaputa
+# Matlab Zynq Server Demo
+
+from numpysocket import NumpySocket
+import numpy as np
+import time
+import mmap
+import struct
+import sys, random
+import ctypes
+import copy
+
+npSocket = NumpySocket()
+npSocket.startServer(9999)
+
+print "entering main loop"
+
+# feel free to modify this command structue as you wish.  It might match the 
+# command structure that is setup in the Matlab side of things on the host PC.
+while(1):
+    cmd = npSocket.receiveCmd()
+    print("received cmd from Matlab: " + str(cmd))
+    npSocket.send(np.array(cmd))
+    print("looping back data to Matlab")
+    break
+    
+npSocket.close()
