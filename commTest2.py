33cf8b655ade2c7d8b3633740525a981ff257729
jab7347 <148905757+jab7347@users.noreply.github.com>
2026-05-01 01:07:56 -0400

    Add files via upload

--- commTest2.py
+++ commTest2.py
@@ -5,21 +5,20 @@ import numpy as np
 npSocket = NumpySocket()
 npSocket.startServer(9999)
 
-
 #Command Loop Structure
 
-while True:
+while(1):
     matLabCmd = npSocket.receiveCmd()
     print(matLabCmd)
     if matLabCmd == '0':
-        #print("SENDING LEFT COMMAND")
-        #npSocket.send(np.array(matLabCmd))
+        print "reciving images"
         dataImg = npSocket.receive()
-        print("GOT Images")
-    elif matLabCmd == '1':
-        print("SENDING POSITION")
-        npSocket.send(np.array([400, 400]))
+        stereoImage = np.reshape(dataImg, (720, 1280, 2))
+        print "GOT Images"
     elif matLabCmd == '2':
+        print "SENDING POSITION"
+        npSocket.send(np.array([400, 400]))
+    elif matLabCmd == '3':
         break
     #End if
 npSocket.close()
