d6f846d97097f175a9c47291f48e0b912951172e
jab7347 <148905757+jab7347@users.noreply.github.com>
2026-05-01 01:42:10 -0400

    Add files via upload

--- ct5.py
+++ ct5.py
@@ -14,11 +14,15 @@ while (1):
     if cmd == '0':
         print "received frame from matlab"
         data = npSocket.receive()
+        stereoImage = np.reshape(data, (720, 1280, 2))
         print "converted image"
     elif cmd == '2':
         print "sending processed frames to matlab"
-        time.sleep(1)
-        npSocket.send(np.array([400,400]))
+        leftGray = stereoImage[:, :, 1]
+        leftGray = np.ascontiguousarray(leftGray, dtype=np.uint8)
+        rightGray = stereoImage[:, :, 2]
+        rightGray = np.ascontiguousarray(rightGray, dtype=np.uint8)
+        npSocket.send(np.array(['400','400']))
     else:
         print "received unknown command"
         break
