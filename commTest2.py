072881648be7f50a0bf30053565f31e95830611e
jab7347 <148905757+jab7347@users.noreply.github.com>
2026-04-30 23:09:13 -0400

    Add files via upload

--- commTest2.py
+++ commTest2.py
@@ -12,10 +12,14 @@ while True:
     matLabCmd = npSocket.receiveCmd()
     print(matLabCmd)
     if matLabCmd == 0:
+        print("SENDING LEFT COMMAND")
         npSocket.send(np.array(1))
         leftImg = npSocket.receive()
+        print("GOT LEFT IMG")
         npSocket.send(np.array(2))
         rightImg = npSocket.receive()
+        print("GOT RIGHT IMG")
+        print("SENDING POSITION")
         npSocket.send(np.array([400,400]))
     elif matLabCmd == 99:
         break
