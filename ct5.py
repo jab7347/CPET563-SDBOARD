2886dcc694b5d361fc40ed5d855ca2830fee1c01
jab7347 <148905757+jab7347@users.noreply.github.com>
2026-05-01 01:55:06 -0400

    Add files via upload

--- ct5.py
+++ ct5.py
@@ -18,11 +18,23 @@ while (1):
         print "converted image"
     elif cmd == '2':
         print "sending processed frames to matlab"
-        leftGray = stereoImage[:, :, 1]
+        leftGray = stereoImage[:, :, 0]
         leftGray = np.ascontiguousarray(leftGray, dtype=np.uint8)
-        rightGray = stereoImage[:, :, 2]
+        rightGray = stereoImage[:, :, 1]
         rightGray = np.ascontiguousarray(rightGray, dtype=np.uint8)
-        npSocket.send(np.array(['400','400']))
+
+        leftEdge = cv2.Sobel(leftGray, cv2.CV_8U, 1, 0, ksize=3, scale=1, delta=0, borderType=cv2.BORDER_DEFAULT)
+        rightEdge = cv2.Sobel(rightGray, cv2.CV_8U, 1, 0, ksize=3, scale=1, delta=0, borderType=cv2.BORDER_DEFAULT)
+
+        num_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(leftEdge, connectivity=8, ltype=cv2.CV_32S)
+        resPos= np.array(['1','1'],dtype=np.uint8)
+        for i in range(1, num_labels):
+            x, y, w, h, area = stats[i]
+            if (abs(w-h) < 25 ) and 100 < area < 200:
+                resPos = np.array([x,y], dtype=np.uint8)
+            #End if
+        #End for
+        npSocket.send(resPos)
     else:
         print "received unknown command"
         break
