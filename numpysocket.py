7e157d9f9246bf0a108282516aa217384adce557
jab7347 <148905757+jab7347@users.noreply.github.com>
2026-05-01 01:44:08 -0400

    Add files via upload

--- numpysocket.py
+++ numpysocket.py
@@ -97,7 +97,7 @@ class NumpySocket():
             socket = self.client_connection
 
         #length = None
-        length = 921600*8
+        length = 921600*16
         frameBuffer = bytearray()
         while True:
             #data = socket.recv(socket_buffer_size)
